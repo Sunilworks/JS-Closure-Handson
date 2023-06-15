e8fbd0f615d99c60e811b91c3aa6fec396de98c0
Sunilworks <sunil.kumar.sh9217@gmail.com>
2023-06-15 16:06:46 +0530

    Handson

---
+++ index.js
@@ -0,0 +1,42 @@
+// let count = 0;
+//             (function () {
+//             if (count === 0) {
+//                     let count = 1;
+//             console.log(count); // What is logged?
+//                 }
+//                 console.log(count); // What is logged?
+//                 })();
+
+
+
+// for (var i = 0; i < 3; i++) {
+//     setTimeout(function log() {
+//       console.log(i); // What is logged?
+//     }, 1000);
+//   }
+
+// function Abc(L){
+//     function Def(B){
+//         return (L*B)
+//     }
+//     return B
+// }
+
+// console.log('Area of rectangle' +" "+ (10*5) )
+
+
+
+// var a = 12;
+// (function () {
+//   alert(a);
+// })();
+
+// var a = 10;
+// var x = (function () {
+//   var a = 12;
+//   return function () {
+//     alert(a);
+//   };
+//     })();
+//     x();
+
